1c83250a2dcb991ffac705387c1339436f5d8807
Caline BRUNO <b.caline@outlook.com>
2025-02-01 10:44:21 +0100

    commentaire et variable css

--- scripts/dropdown.js
+++ scripts/dropdown.js
@@ -1,18 +1,24 @@
+// On récupère les éléments ciblés dans des variables.
 const dropdown = document.querySelector(".dropdown");
 const categories = document.querySelector(".categories");
 const categoryItems = document.querySelectorAll(".categories li");
 const products = document.querySelectorAll(".product");
 
-
+// On cherche tous les éléments de notre liste déroulante.
 const selectedItem = document.querySelector(".categories .selected");
 if (selectedItem) {
     dropdown.textContent = selectedItem.textContent;
 }
 
+// Au clic sur la div qui regoupe la liste du menu
+// la classe active s'ajoute.
 dropdown.addEventListener("click", function () {
     categories.classList.toggle("active");
 });
 
+// Pour chaque éléments de la classe categorie, 
+// on ajoute un écouteur d'évènement afin qu'au clic
+//  d'un d'entre eux la classe active se retire et le menu se ferme.
 categoryItems.forEach(item => {
     item.addEventListener("click", function () {
         dropdown.textContent = this.textContent;
@@ -20,11 +26,17 @@ categoryItems.forEach(item => {
     });
 });
 
+// Pour chaque élément du menu déroulant
 categoryItems.forEach(item => {
     item.addEventListener("click", function () {
+        // On récupère la 2em class
+        // afin de le lier à la class correspondante aux produits
         const selectedCategory = this.classList[1];
         dropdown.textContent = this.textContent;
 
+        // Pour chaque produit, on vérifie s'il appartient à une class spécifique ou la class commune à tous (all)
+        // Et on affiche les produits ayant la class sélectionné dans le menu
+        // Ou on n'affiche rien si on n'a pas de produits correspondant à l'élément dans le menu déroulant
         products.forEach(product => {
             if (product.classList.contains(selectedCategory) || selectedCategory === "all") {
                 product.style.display = "list-item";
@@ -35,6 +47,9 @@ categoryItems.forEach(item => {
     });
 });
 
+// Si on clic en dehors du menu déroulant ou pas sur un de ses éléments "li"
+// lorsqu'il est ouvert, il se referme et 
+// on ne change rien aux produits affichés.
 document.addEventListener("click", function (event) {
     if (!dropdown.contains(event.target) && !categories.contains(event.target)) {
         categories.classList.remove("active");
--- scripts/navbar.js
+++ scripts/navbar.js
@@ -1,13 +1,18 @@
+// On récupère les éléments ciblés dans des variables.
 const menu_btn = document.querySelector(".burger_menu");
 const navbar = document.querySelector(".navbar");
 
+// On ajoute un écouteur d'évènement sur l'icon via sa class récupéré dans la variable.
 menu_btn.addEventListener("click", () => {
     navbar.classList.toggle("active");
 });
 
-
+// On ferme le menu si on click en dehors du menu ou de la navbar
 document.addEventListener("click", function (event) {
     if (!menu_btn.contains(event.target) && !navbar.contains(event.target)) {
         navbar.classList.remove("active");
     }
 });
+
+
+// Pour aller sur la page correspondante, nos "li", sont aussi des liens vers les pages "a href".
