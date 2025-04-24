8271d535a88eb5bdb1ce903048ed26d2ae23f820
Caline BRUNO <b.caline@outlook.com>
2025-04-24 23:20:13 +0200

    minor fixes

---
+++ public/scripts/prod_search.js
@@ -0,0 +1,15 @@
+    const searchInput = document.getElementById("searchInput");
+
+    searchInput.addEventListener("input", () => {
+        const query = searchInput.value.toLowerCase();
+        const products = document.querySelectorAll(".product");
+
+        products.forEach(product => {
+            const name = product.querySelector(".prod_name").textContent.toLowerCase();
+            if (name.startsWith(query)) {
+                product.style.display = "block";
+            } else {
+                product.style.display = "none";
+            }
+        });
+    });
